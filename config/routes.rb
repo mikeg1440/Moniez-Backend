3b12d4eb488b7149128ea4d318972e37b1ed122a
Mike Gaudreau <gaudreaum01@gmail.com>
2020-02-12 17:37:33 -0500

    Add routes for sessions and users namespaced within API/V1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  # devise_for :users
+
+  namespace :api do
+    namespace :v1 do
+
+      resources :sessions, only: [:create, :destroy]
+      resources :users, only: [:create, :update, :destroy]
+      resources :registrations, only: [:create]
+
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
