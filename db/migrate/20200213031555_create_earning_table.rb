ca5e8484004c42045eb15b9c554f1b393aee6b61
Mike Gaudreau <gaudreaum01@gmail.com>
2020-02-13 13:54:27 -0500

    Add earnings table migration

---
+++ db/migrate/20200213031555_create_earning_table.rb
@@ -0,0 +1,9 @@
+class CreateEarningTable < ActiveRecord::Migration[6.0]
+  def change
+    create_table :earnings do |t|
+      t.references :budgets, null: false, foreign_key: true
+      t.references :earning_category, null: false, foreign_key: true
+      t.float :amount
+    end
+  end
+end
