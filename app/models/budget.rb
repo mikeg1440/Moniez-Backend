c30e087e5d9dad7105859cdb1eeedf929c97ac34
Mike Gaudreau <gaudreaum01@gmail.com>
2020-02-13 14:23:51 -0500

    Delete the git message lines that somehow got saved in the budget model file

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,14 +1,10 @@
 class Budget < ApplicationRecord
   belongs_to :user
-<<<<<<< Updated upstream
-
-=======
   has_many :earnings
   has_many :expenses
   has_many :bills
 
-  
->>>>>>> Stashed changes
+
   validates :title, presence: true
 
 end
