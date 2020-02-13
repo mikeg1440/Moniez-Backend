11d298f4b4f5a03f38f74faa445bc5adea61ca14
Mike Gaudreau <gaudreaum01@gmail.com>
2020-02-12 21:34:13 -0500

    Refactor users model to work with simple tokens instead of devise-jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
+  acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # devise :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :validatable
-  devise :database_authenticatable, :jwt_authenticatable#, jwt_revocation_strategy: JWTBlacklist
+  devise :database_authenticatable#, :jwt_authenticatable, jwt_revocation_strategy: JWTBlacklist
 
   validates :email, presence: true, uniqueness: true
   validates :username, presence:true, uniqueness: true
-  validates :password, presence: true
+  # validates :password, presence: true
 
 end
