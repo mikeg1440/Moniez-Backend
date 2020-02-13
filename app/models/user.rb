948079e9ca0cec2048bc809ca456b872aec4ec97
Mike Gaudreau <gaudreaum01@gmail.com>
2020-02-12 22:09:02 -0500

    Add budget relationship to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :budgets
+
   acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
