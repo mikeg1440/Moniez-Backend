5bf510ee7b1d57c19e1955f96c5bd6b1fb680433
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-14 15:12:12 -0500

    Remove the user.save line in sessions controller because it does nothing

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: session_params[:email])
     if user&.valid_password?(session_params[:password])
-      user.save
       render json: user.as_json(only: [:id, :email, :username, :authentication_token])
     else
       head(:unauthorized)
