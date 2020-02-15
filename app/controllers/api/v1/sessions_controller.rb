06b06e2effdf2c04c086faea4adf5f06f8d4a1be
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-15 10:04:09 -0500

    Add logout action to sessions controller and rename the create action to login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,11 +1,23 @@
 class Api::V1::SessionsController < ApplicationController
 
-  def create
+  def login
     user = User.find_by(email: session_params[:email])
     if user&.valid_password?(session_params[:password])
+      response.set_header('bearer', user.authentication_token)
       render json: user
     else
       render json: {errors: 'Incorrect Authentication'}, status: :unauthorized
+    end
+  end
+
+  def logout
+    user = User.find_by(authentication_token: request.headers[:bearer])
+    if user
+      user.authentication_token.clear
+      response.delete_token(:bearer)
+      head(:ok)
+    else
+      head(:not_acceptable)
     end
   end
 
