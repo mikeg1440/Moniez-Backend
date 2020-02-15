8f3f4fba8634f5481ffcdb5986ee129629ceda03
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-15 10:24:41 -0500

    Add a before action to application controller called authenticate_user to check if there is a valid token in the header before allowing access to any controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user
+
+  def authenticate_user
+    if auth_header
+      @user = User.find_by(authentication_token: auth_header)
+      @user
+    else
+      head(:unauthorized)
+    end
+  end
+
+  private
+
+  def auth_header
+    headers[:bearer]
+  end
+
 end
