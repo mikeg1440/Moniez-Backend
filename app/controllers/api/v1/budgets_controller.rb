80de61f187d42f87df183588a3351b022e3bd2ad
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-15 12:39:07 -0500

    Add logic for create and destroy actions in the budgets controller

--- app/controllers/api/v1/budgets_controller.rb
+++ app/controllers/api/v1/budgets_controller.rb
@@ -5,13 +5,31 @@ class Api::V1::BudgetsController < ApplicationController
   end
 
   def create
-
+    new_budget = @user.budgets.build(budget_params)
+    if new_budget.valid?
+      new_budget.save
+      render json: new_budget, status: :created
+    else
+      render json: {errors: new_budget.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def update
   end
 
   def destroy
+    budget = Budget.find_by(id: params[:id])
+    if budget&.destroy
+      render json: budget, status: :ok
+    else
+      render json: {errors: "Failed to delete budget"}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def budget_params
+    params.require(:budget).permit(:title, :description)
   end
 
 end
