a1ffad2b4c29134e495b00aa6ec08104e4d06e0f
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-15 10:13:12 -0500

    Refactor routes to reflect the update of the new controller action names

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,12 @@ Rails.application.routes.draw do
       # resources :sessions, only: [:create, :destroy]
       post '/login', to: '/api/v1/sessions#login', as: 'user_login'
       get '/logout', to: '/api/v1/sessions#logout', as: 'user_logout'
-      
-      resources :users, only: [:create, :update, :destroy]
-      resources :registrations, only: [:create]
+
+      # resources :users, only: [:create, :update, :destroy]
+      # resources :registrations, only: [:create]
+      post '/register', to: '/api/v1/users#register_user', as: 'user_registration'
+      put '/user', to: '/api/v1/users#update_user', as: 'update_user'
+      get '/user/delete', to: '/api/v1/users#destroy_user', as: 'delete_user'
 
     end
   end
