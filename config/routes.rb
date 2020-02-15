934ada0e9c545159d6d0db135cd4cd8c6d2429fc
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-15 10:03:36 -0500

    Refactor routes for session login and logout to use those terms in the url

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
-      resources :sessions, only: [:create, :destroy]
+      # resources :sessions, only: [:create, :destroy]
+      post '/login', to: '/api/v1/sessions#login', as: 'user_login'
+      get '/logout', to: '/api/v1/sessions#logout', as: 'user_logout'
+      
       resources :users, only: [:create, :update, :destroy]
       resources :registrations, only: [:create]
 
