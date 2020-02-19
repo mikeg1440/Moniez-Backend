2b60706ef3a4eaef9080a000f0debde8ee8fe3a0
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-19 17:29:00 -0500

    Add a update_total after update function to the budget model to update the total whenever they update

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,4 +1,6 @@
 class Budget < ApplicationRecord
+  after_update :update_total
+
   belongs_to :user
   has_many :earnings
   has_many :expenses
@@ -6,5 +8,13 @@ class Budget < ApplicationRecord
 
 
   validates :title, presence: true
+
+  def update_total
+    total = self.earnings.all.sum{|earning| earning.amount }
+    total -= self.bills.all.sum{|bill| bill.amount}
+    total -= self.expenses.all.sum{|expense| expense.amount}
+    self.total = total
+  end
+
 
 end
