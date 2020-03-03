20803a8edce64bd4f0d22006de531bb9173f750c
Michael Gaudreau <gaudreaum01@gmail.com>
2020-03-02 19:33:06 -0500

    Refactor budget_serializer so that it returns each of the budget categories in sorted descending order by amount

--- app/serializers/budget_serializer.rb
+++ app/serializers/budget_serializer.rb
@@ -3,19 +3,19 @@ class BudgetSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :total, :earnings, :expenses, :bills
 
   def earnings
-    earnings = Earning.where(budget_id: self.object.id)
+    earnings = Earning.where(budget_id: self.object.id).order(amount: :desc)
     earnings_object = earnings.map {|earning| EarningSerializer.new(earning).as_json}
     earnings_object
   end
 
   def expenses
-    expenses = Expense.where(budget_id: self.object.id)
+    expenses = Expense.where(budget_id: self.object.id).order(amount: :desc)
     expenses_object = expenses.map {|expense| ExpenseSerializer.new(expense).as_json}
     expenses_object
   end
 
   def bills
-    bills = Bill.where(budget_id: self.object.id)
+    bills = Bill.where(budget_id: self.object.id).order(amount: :desc)
     bills_object = bills.map {|bill| BillSerializer.new(bill).as_json}
     bills_object
   end
