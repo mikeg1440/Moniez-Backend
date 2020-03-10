837175327f511dbfc96cca4dc03d36f3880eec62
Michael Gaudrau <gaudreaum01@gmail.com>
2020-03-10 16:02:59 -0400

    Add tests for user relationships to earnings, expenses and bills

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,7 +33,43 @@ RSpec.describe User, type: :model do
 
   end
 
+  context 'budgets' do
+
+    user = User.last
+
+    # create 4 new budgets for user
+    (1..4).each {|num| user.budgets.build(title: "#{Faker::Name.first_name}'s Budget'", description: 'A budget to make sure everything is working correctly.') }
+    user.save
+
+    budget = user.budgets.first
+
+    # create 5 new bills for budget
+    (1..5).map {|num| budget.earnings.build(earning_category: EarningCategory.all.sample, amount: rand(1000)) }
+    (1..5).map {|num| budget.bills.build(bill_category: BillCategory.all.sample, amount: rand(1000)) }
+    (1..5).map {|num| budget.expenses.build(expense_category: ExpenseCategory.all.sample, amount: rand(1000)) }
+    budget.save
+
+
+    it 'has many budgets' do
+      expect(user.budgets.count).to eq(4)
+    end
+
+    it 'has many earnings through budgets' do
+      expect(user.earnings.count).to eq(5)
+    end
+
+    it 'has many bills through budgets' do
+      expect(user.bills.count).to eq(5)
+    end
+
+    it 'has many expenses through budgets' do
+      expect(user.expenses.count).to eq(5)
+    end
+
+
   end
+
+
 
 
 end
