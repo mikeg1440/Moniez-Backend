8e7f65c95b94e99f4605bbeb5f6ac36f3557a803
Michael Gaudrau <gaudreaum01@gmail.com>
2020-03-11 17:41:05 -0400

    Add after update and delete actions for expense, bill and earnings models

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,6 +1,14 @@
 class Bill < ApplicationRecord
+  after_create :update_budget
+  after_destroy :update_budget
+
   belongs_to :budget
   belongs_to :bill_category
 
   validates :amount, presence: true
+
+  def update_budget
+    self.budget.update_total
+  end
+  
 end
--- app/models/earning.rb
+++ app/models/earning.rb
@@ -1,7 +1,14 @@
 class Earning < ApplicationRecord
+  after_create :update_budget
+  after_destroy :update_budget
+
   belongs_to :budget
   belongs_to :earning_category
 
   validates :amount, presence: true
+
+  def update_budget
+    self.budget.update_total
+  end
 
 end
