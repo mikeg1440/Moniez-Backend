5aff29ac59c22bcd7dbdc7f143604f6cd0680249
Michael Gaudrau <gaudreaum01@gmail.com>
2020-03-11 17:51:24 -0400

    Add tests to budget spec for testing update of total after adding and deleting entries

--- spec/models/budget_spec.rb
+++ spec/models/budget_spec.rb
@@ -40,4 +40,39 @@ RSpec.describe Budget, type: :model do
 
   end
 
+  context 'updates the total after update' do
+
+    budget = user.budgets.first
+
+    # expense = budget.expenses.build(expense_category_id: 7, amount: rand(500))
+    # bill = budget.bills.build(bill_category_id: 7, amount: rand(500))
+
+    budget.update_total
+    # original_total = budget.total
+    # earning = budget.earnings.build(earning_category_id: 1, amount: rand(1500))
+    # budget.save
+
+    it 'after adding earning' do
+      original_total = budget.total
+      earning = budget.earnings.build(earning_category_id: 1, amount: rand(1500))
+      budget.save
+      expect(budget.total * 1.0).to equal(original_total + earning.amount)
+    end
+
+    it 'after removing earning' do
+      original_total = budget.total
+      earning = budget.earnings.last
+      earning.destroy
+      expect(budget.total* 1.0).to eq(original_total - earning.amount)
+    end
+
+    it 'after adding expense'
+
+    it 'after removing expense'
+
+    it 'after adding bill'
+
+    it 'removing expense'
+
+  end
 end
